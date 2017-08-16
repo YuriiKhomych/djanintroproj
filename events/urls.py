80bef8d4856a22aac5469222fb5d49802b0a8004
yuriikhomych <yuriykhomich@gmail.com>
2017-08-16 17:28:30 +0300

    Create CBV for add event

--- events/urls.py
+++ events/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from events.views import events_list, single_event, add_event, join_member
+from events.views import events_list, single_event, join_member, AddEvent, AllEvents
+
 
 urlpatterns = [
-    url(r'^$', events_list, name='all_events'),
-    url(r'^add-event/$', add_event, name='add_event'),
+    url(r'^add-event/$', AddEvent.as_view(), name='add_event'),
+    url(r'^$', AllEvents.as_view(), name='all_events'),
+    # url(r'^add-event/$', add_event, name='add_event'),
     url(r'^single/(?P<event_id>\d+)/$', single_event, name='single_event_page'),
     url(r'^single/(?P<event_id>\d+)/join-member/$', join_member, name='join_member'),
 ]
-
--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect, reverse
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.utils import timezone
+from django.views.generic import View
 
 from events.models import Event
 from .forms import CreateNewEvent
@@ -41,21 +42,21 @@ def single_event(request, event_id):
     return render(request, 'single-event.html', {'event': event})
 
 
-def add_event(request):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            form = CreateNewEvent(request.POST)
-            if form.is_valid():
-                event = form.save(commit=False)
-                event.creator = request.user
-                event.date = timezone.now()
-                event.save()
-                return redirect('all_events')
-        else:
-            form = CreateNewEvent()
-        return render(request, 'add-event.html', {'form': form})
-    else:
-        return HttpResponseRedirect(reverse('all_events'))
+# def add_event(request):
+#     if request.user.is_authenticated:
+#         if request.method == 'POST':
+#             form = CreateNewEvent(request.POST)
+#             if form.is_valid():
+#                 event = form.save(commit=False)
+#                 event.creator = request.user
+#                 event.date = timezone.now()
+#                 event.save()
+#                 return redirect('all_events')
+#         else:
+#             form = CreateNewEvent()
+#         return render(request, 'add-event.html', {'form': form})
+#     else:
+#         return HttpResponseRedirect(reverse('all_events'))
 
 
 def join_member(request, event_id):
@@ -66,34 +67,36 @@ def join_member(request, event_id):
         else:
             event.members.add(request.user)
             event.save()
-        return HttpResponseRedirect(reverse('all_events'))
+        return HttpResponseRedirect(reverse('single_event_page', kwargs={'event_id': event_id}))
     else:
         return HttpResponseRedirect(reverse('sign_up'))
 
-# class Event(View):
-#     form_class = Event
-#
-#     def get(self, request):
-#         event = self.form_class.objects.all()
-#         page = request.GET.get('page', 1)
-#         p = Paginator(event, 1)
-#         try:
-#             final_events = p.page(page)
-#         except PageNotAnInteger:
-#             # If page is not an integer, deliver first page.
-#             final_events = p.page(1)
-#         except EmptyPage:
-#             # If page is out of range (e.g. 9999), deliver last page of results.
-#             final_events = p.page(p.num_pages)
-#         return render(request, 'events.html', {'events': final_events,
-#                                                'pagination': gen_page_list(page, p.num_pages)})
-#
-#     def post(self,request):
-#         form = CreateNewEvent(request.POST)
-#         if form.is_valid():
-#             event = form.save(commit=False)
-#             event.creator = request.user
-#             event.date = timezone.now()
-#             event.save()
-#             return redirect('single_event_page', event_id=event.pk)
-#         return render(request, 'add-event.html', {'form': form})
+class AllEvents(View):
+
+    def get(self, request):
+        event = Event.objects.all()
+        page = request.GET.get('page', 1)
+        p = Paginator(event, 1)
+        try:
+            final_events = p.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            final_events = p.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            final_events = p.page(p.num_pages)
+        return render(request, 'events.html', {'events': final_events,
+                                               'pagination': gen_page_list(page, p.num_pages)})
+
+
+class AddEvent(View):
+
+    def post(self, request):
+        form = CreateNewEvent(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.creator = request.user
+            event.date = timezone.now()
+            event.save()
+            return redirect('all_events')
+        return render(request, 'add-event.html', {'form': form})
