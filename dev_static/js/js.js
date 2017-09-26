1cd758c9247aa917cf094642d529146d48a2ca73
yuriikhomych <yuriykhomich@gmail.com>
2017-09-26 22:46:18 +0300

    Try use google place autocomplete

--- dev_static/js/js.js
+++ dev_static/js/js.js
@@ -2,3 +2,6 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('select').material_select();
 });
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
