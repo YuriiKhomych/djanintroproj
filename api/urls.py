196e436062f76c060240aa2fc7d6c9a78d2428e0
yuriikhomych <yuriykhomich@gmail.com>
2017-10-12 22:56:08 +0300

    Change ArticleRemoveAPI to ArticleRetrieveUpdateDestroyAPI with RetrieveUpdateDestroyAPIView

--- api/urls.py
+++ api/urls.py
@@ -12,7 +12,7 @@ from .views import (
     ArticlesView,
     ArticlesAllAPI,
     ArticleCreateAPI,
-    ArticleRemoveAPI,
+    ArticleRetrieveUpdateDestroyAPI,
     ArticlesSearchAPI,
     # trip
     TripView,
@@ -36,7 +36,7 @@ urlpatterns = [
     url(r'^articles-rest/(?P<pk>\d+)/$', ArticlesView.as_view()),
     url(r'^articles-all-api/$', ArticlesAllAPI.as_view()),
     url(r'^articles-create-api/$', ArticleCreateAPI.as_view()),
-    url(r'^articles-remove-api/(?P<pk>\d+)/$', ArticleRemoveAPI.as_view()),
+    url(r'^articles-retrieve-update-destroy-api/(?P<pk>\d+)/$', ArticleRetrieveUpdateDestroyAPI.as_view()),
     url(r'^articles-search-api/$', ArticlesSearchAPI.as_view()),
     # trip API
     url(r'^trip-rest/$', TripView.as_view()),
