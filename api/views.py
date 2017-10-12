86a5dc77383acb2bed997b05b1c28e185fd5af7b
yuriikhomych <yuriykhomich@gmail.com>
2017-10-12 22:37:02 +0300

    Cleanup

--- api/views.py
+++ api/views.py
@@ -15,6 +15,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import (
+    GenericAPIView,
+    ListAPIView,
     ListCreateAPIView,
     DestroyAPIView
 )
